9e6c09b63bca1bc2c78c03eff19c4dd5b0ce3c00
maziazi <mazis9651@gmail.com>
2025-07-16 13:59:06 +0700

    Initial Commit

---
+++ tesTests/tesTests.swift
@@ -0,0 +1,17 @@
+//
+//  tesTests.swift
+//  tesTests
+//
+//  Created by Muhamad Azis on 16/07/25.
+//
+
+import Testing
+@testable import tes
+
+struct tesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
